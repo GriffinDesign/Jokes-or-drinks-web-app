a32cb6cede288b569d7759854756bc5ad0bbcea3
Nick-Menzhuber <nickmenzhuber@gmail.com>
2022-10-13 17:14:26 -0500

    attached js file to html and added all api calls to js file, logging objects to console

---
+++ script.js
@@ -0,0 +1,34 @@
+var funFactAPI = "https://asli-fun-fact-api.herokuapp.com/";
+var dadJokeAPI = "https://icanhazdadjoke.com";
+var cocktailAPI = "https://thecocktaildb.com/api/json/v1/1/random.php";
+
+fetch(funFactAPI)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+fetch(dadJokeAPI, {
+  headers : { 
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+   }
+})
+  .then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+})
+
+
+
+fetch(cocktailAPI)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
