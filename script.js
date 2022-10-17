27266e89b7db95bb802e7b9cc04462f673c90dda
Nick-Menzhuber <nickmenzhuber@gmail.com>
2022-10-17 13:28:48 -0500

    connecting work from pr with new push

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-var funFactAPI = "https://asli-fun-fact-api.herokuapp.com/";
+$(document).ready(function () {
+  let funFact = document.getElementById("fun-fact-text")
+  let startBtn = document.getElementsByClassName("start-button")
+  let welcomeText = document.getElementsByClassName(".welcome-text")
+  let dadJokeBtn = "<button class=\"dad-joke-button\">I need a dad joke</button>"
+  function factFetch() {
+      const funFactAPI = "https://asli-fun-fact-api.herokuapp.com/"
+      fetch(funFactAPI)
+          .then(function (response) {
+              return response.json();
+          })
+          .then(function (data) {
+              console.log(data.data.fact);
+              $(funFact).text(data.data.fact);
+
+          })
+  }
+
+})
+/*var funFactAPI = "https://asli-fun-fact-api.herokuapp.com/";
 var dadJokeAPI = "https://icanhazdadjoke.com";
 var cocktailAPI = "https://thecocktaildb.com/api/json/v1/1/random.php";
 
@@ -32,3 +51,4 @@ fetch(cocktailAPI)
   .then(function (data) {
     console.log(data);
   });
+  */
