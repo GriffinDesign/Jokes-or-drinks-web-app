f33545eee7052c65b87a931029dbd403505b8612
Nick-Menzhuber <nickmenzhuber@gmail.com>
2022-10-17 20:44:52 -0500

    fixed dad joke button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   let funFact = $(".fun-fact-text");
   let dadJokeBtn = $(".needs-dad-joke");
   let dadJokeText = $(".dad-joke-text")
+  let noBtn = $(".no-button")
   function factFetch() {
     const funFactAPI = "https://asli-fun-fact-api.herokuapp.com/"
     fetch(funFactAPI)
@@ -40,10 +41,12 @@ function dadJokeFetch() {
 $(startBtn).on("click", function () {
     factFetch();
     $(welcomeText).remove();
+    $(noBtn).remove();
     $(startBtn).text("Show another fact!");
 })
 $(dadJokeBtn).on("click", function () {
   dadJokeFetch();
+  $(dadJokeBtn).text("Show another dad joke!");
 
 }) 
 })
