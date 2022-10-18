a009a52d30d94f9a57764086719f2535b0b473dc
Nick-Menzhuber <nickmenzhuber@gmail.com>
2022-10-17 19:34:14 -0500

    gets fun fact to display

--- script.js
+++ script.js
@@ -1,54 +1,25 @@
 $(document).ready(function () {
-  let funFact = document.getElementById("fun-fact-text")
   let startBtn = document.getElementsByClassName("start-button")
   let welcomeText = document.getElementsByClassName(".welcome-text")
-  let dadJokeBtn = "<button class=\"dad-joke-button\">I need a dad joke</button>"
+  let funFact = document.getElementsByClassName("fun-fact-text");
   function factFetch() {
-      const funFactAPI = "https://asli-fun-fact-api.herokuapp.com/"
-      fetch(funFactAPI)
-          .then(function (response) {
-              return response.json();
-          })
-          .then(function (data) {
-              console.log(data.data.fact);
-              $(funFact).text(data.data.fact);
+    const funFactAPI = "https://asli-fun-fact-api.herokuapp.com/"
+    fetch(funFactAPI)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.data.fact);
+            $(funFact).prepend()
+            $(welcomeText).text(data.data.fact);
 
-          })
-  }
 
+        })
+}
+
+$(startBtn).on("click", function () {
+    factFetch();
+    $(welcomeText).remove();
+    $(startBtn).text("Show another fact!");
 })
-/*var funFactAPI = "https://asli-fun-fact-api.herokuapp.com/";
-var dadJokeAPI = "https://icanhazdadjoke.com";
-var cocktailAPI = "https://thecocktaildb.com/api/json/v1/1/random.php";
-
-fetch(funFactAPI)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-
-fetch(dadJokeAPI, {
-  headers : { 
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-   }
 })
-  .then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-})
-
-
-
-fetch(cocktailAPI)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-  */
